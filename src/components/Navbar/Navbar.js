445e0636cac5f25183b82154a32a7abf5b9786ff
aniyy117 <janiket112@gmail.com>
2021-08-10 21:40:13 +0530

    changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { FaBars } from "react-icons/fa";
 import {
   Nav,
@@ -12,30 +12,12 @@ import {
   NavBtnLink,
 } from "./NavbarElements";
 
-import { animateScroll as scroll } from "react-scroll";
 const Navbar = ({ toggle }) => {
-  const [scrollNav, setScrollNav] = useState(false);
-
-  const changeNav = () => {
-    if (window.scrollY >= 80) {
-      setScrollNav(true);
-    } else {
-      setScrollNav(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", changeNav());
-  }, []);
-
-  const toggleHome = () => {
-    scroll.scrollToTop();
-  };
   return (
     <>
-      <Nav scrollNav={scrollNav}>
+      <Nav>
         <NavbarContainer to="home">
-          <NavLogo onClick={toggleHome}>Http.DittoSays</NavLogo>
+          <NavLogo>Http.DittoSays</NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
