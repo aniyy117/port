e57f92b8420f15e766150eaf4726c87692fadbbb
aniyy117 <janiket112@gmail.com>
2021-08-10 12:55:42 +0530

    changes

--- src/components/HeroPage/Hero.js
+++ src/components/HeroPage/Hero.js
@@ -1,13 +1,75 @@
 import React from "react";
-import {
-  HeroContainer,
-  HeroBg,
-  Video,
-  HeroH1,
-  HeroContent,
-  HeroP,
-} from "./HeroElements";
 import video from "../videos/Pexels.mp4";
+import styled from "styled-components";
+
+export const HeroContainer = styled.div`
+  font-family: "Encode Sans Expanded", sans-serif;
+  background: #0c0c0c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  height: 650px;
+  position: relative;
+  z-index: 1;
+  @media screen and (max-width: 768px) {
+    height: 600px;
+  }
+
+  /*Add :before styles*/
+`;
+
+export const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+export const Video = styled.video`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  background: #232a34;
+`;
+
+export const HeroContent = styled.div`
+  z-index: 3;
+  max-width: 900px;
+  position: absolute;
+  padding: 8px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HeroH1 = styled.h1`
+  color: #fff;
+  font-size: 4rem;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
+
+export const HeroP = styled.p`
+  margin-top: 24px;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  max-width: 500px;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 2px;
+    font-size: 1rem;
+  }
+`;
 
 function Hero() {
   return (
